faabff7490e26d483cb8415046e18de415c6198e
tabio <tabio.github@gmail.com>
2022-07-10 22:44:02 +0900

    initial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # overlay-lgtm
 画像にLGTMを差し込むスクリプト
+
+## 準備
+
+```sh
+python3 -m venv .env
+source .env/bin/activate
+pip install -r requirements.txt
+
+* pillowのインストールに brew install libjpeg が必要だった
+* pillowがfontをいじるのに　brew install freetype が必要だった
+```
---
+++ overlay.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+text = 'LGTM'
+
+# target-imagesディレクトリからファイル一覧を取得
+target_images = [f for f in os.listdir('target-images') if os.path.isfile(os.path.join('target-images', f))]
+
+# target_imagesをループして画像を取得して、textを書き込む
+for image_name in target_images:
+    if image_name == '.gitkeep':
+      continue
+
+    # 画像を取得
+    image = Image.open(os.path.join('target-images', image_name))
+
+    # 文字を書き込む
+    draw = ImageDraw.Draw(image)
+
+    image_w = image.size[0]
+    image_h = image.size[1]
+
+    fontsize = int(image_w / 7) # fontサイズは画像の幅に合わせて調整
+
+    # find /Library/Fonts/ で利用できるfontを探す
+    font = ImageFont.truetype('/Library/Fonts//Arial Unicode.ttf', size=fontsize)
+
+    font_w, font_h = draw.textsize(text, font=font)
+
+    draw.text(((image_w - font_w) / 2, (image_h - font_h) / 2), text, font=font, fill='#ffffff')
+
+    # 画像を保存
+    image.save(os.path.join('output-images', image_name))
+    print(f'{image_name} is completed.')
